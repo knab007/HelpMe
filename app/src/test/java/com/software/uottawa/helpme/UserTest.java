90c06d62881b2fe5c37c5904e328741a9d9fbb0d
Botrel Kouam <akoua038@uottawa.ca>
2018-12-05 23:05:50 -0500

    commit TEST REUSSI

--- app/src/test/java/com/software/uottawa/helpme/UserTest.java
+++ app/src/test/java/com/software/uottawa/helpme/UserTest.java
@@ -8,7 +8,7 @@ public class UserTest {
     @Test
     public void checkUserFirstName() {
 
-        User aUser = new User("1","Samir","Tchakounte","samgmail@.com","homeOwner");
+        User aUser = new User("1","Samir","Tchakounte","Sam@gmail.com","homeOwner");
         assertEquals("Check User firstName", "Samir",aUser.getFirstName());
 
     }
@@ -16,7 +16,7 @@ public class UserTest {
     @Test
     public void checkUserLastName() {
 
-        User aUser = new User("1","Samir","Tchakounte","samgmail@.com","homeOwner");
+        User aUser = new User("1","Samir","Tchakounte","Sam@gmail.com","homeOwner");
         assertEquals("Check User LastName", "Tchakounte",aUser.getLastName());
 
     }
@@ -24,15 +24,15 @@ public class UserTest {
     @Test
     public void checkUserEmail() {
 
-        User aUser = new User("1","Samir","Tchakounte","Samgmail@.com","homeOwner");
-        assertEquals("Check User Email", "Samgmail.com",aUser.getEmail());
+        User aUser = new User("1","Samir","Tchakounte","Sam@gmail.com","homeOwner");
+        assertEquals("Check User Email", "Sam@gmail.com",aUser.getEmail());
 
     }
 
     @Test
     public void checkUserType() {
 
-        User aUser = new User("1","Samir","Tchakounte","samgmail@.com","homeOwner");
+        User aUser = new User("1","Samir","Tchakounte","Sam@gmail.com","homeOwner");
         assertEquals("Check User Type", "homeOwner",aUser.getTypeOfUser());
 
     }
@@ -40,7 +40,7 @@ public class UserTest {
     @Test
     public void checkUserId() {
 
-        User aUser = new User("1","Samir","Tchakounte","samgmail@.com","homeOwner");
+        User aUser = new User("1","Samir","Tchakounte","Sam@gmail.com","homeOwner");
         assertEquals("Check User Id", "1",aUser.getId());
 
     }
